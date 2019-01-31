6cefab5c118e00af9df9e03af67d5d579c62b069
SoniaMahankali <mahankali.s@northeastern.edu>
2019-01-31 12:29:19 -0800

    controller-test

---
+++ webapp/src/main/java/com/howtodoinjava/rest/Service/IUserService.java
@@ -0,0 +1,19 @@
+package com.howtodoinjava.rest.Service;
+
+import com.howtodoinjava.rest.model.User;
+
+import java.util.List;
+
+public interface IUserService {
+    int add(User account);
+
+    int update(User account);
+
+    int delete(int id);
+
+    User findAccountById(int id);
+
+    User findAccountByName(String name);
+
+    List<User> findAccountList();
+}
---
+++ webapp/src/main/java/com/howtodoinjava/rest/SpringBootDemoApplication.java
@@ -0,0 +1,14 @@
+package com.howtodoinjava.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+}
