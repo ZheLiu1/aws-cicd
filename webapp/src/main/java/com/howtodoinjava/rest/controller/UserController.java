b26013ad767992128aae77f643359993a8e877a6
Zhe <liu.zhe3@husky.neu.edu>
2019-02-05 13:09:44 -0500

    clean

--- webapp/src/main/java/com/howtodoinjava/rest/controller/UserController.java
+++ webapp/src/main/java/com/howtodoinjava/rest/controller/UserController.java
@@ -1,29 +1,16 @@
 package com.howtodoinjava.rest.controller;
 
 import java.io.IOException;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import com.codahale.passpol.BreachDatabase;
 import com.codahale.passpol.PasswordPolicy;
 
 
 import com.howtodoinjava.rest.dao.IUserDAO;
-import com.howtodoinjava.rest.dao.UserDaoImpl;
 import com.howtodoinjava.rest.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-
-
-import javax.annotation.Resource;
 
 @RestController
 public class UserController
@@ -32,23 +19,6 @@ public class UserController
     IUserDAO accountService;
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
-
-  /*  @RequestMapping(value = "/")
-    public String hello(@RequestHeader(value="Authorization") String comingM){
-        String[] userInfo = decodeBase64(comingM);
-        String user_name = userInfo[0];
-        String user_password = userInfo[1];
-        String returnM = null;
-
-        if(verify(user_name,user_password)){
-            java.text.SimpleDateFormat df = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            returnM = df.format(System.currentTimeMillis());
-        }else
-            returnM = "You are not logged in!";
-        return returnM;
-
-    }*/
-
 
     @RequestMapping(value = "/")
     public String httpGet(@RequestHeader(value="Authorization") String comingM){
@@ -91,14 +61,6 @@ public class UserController
         return userInfo;
     }
 
-
-    /*@PostMapping("/saveuser")
-    public User newCreate(@RequestBody List<String> str){
-
-    }
-
-*/
-
     public boolean isValid(String email){
 
         String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
@@ -113,46 +75,10 @@ public class UserController
 
     }
 
-        // checking for header
-   @RequestMapping(value = "/user/registertest")
-    public String addUsertest(@RequestHeader(value="Authorization") String comingM) {
-       String[] userInfo = decodeBase64(comingM);
-       String user_name = userInfo[0];
-       String user_password = userInfo[1];
-
-       // 2: Duplicate username done
-       User user = accountService.findAccountByName(user_name);
-       if (user.getUser_name().equalsIgnoreCase(user_name)) {
-           return "Duplicate Value enter again!!";
-       }
-
-       //3 : strong password code
-       final PasswordPolicy policy = new PasswordPolicy(BreachDatabase.haveIBeenPwned(5), 8, 64);
-
-       System.out.print("The message is:" + policy.check("Kailash@201231"));
-
-       // 1: check username for email in proper format
-
-       if (isValid(user_name)) {  //done
-           User use = new User();
-           use.setUser_id(14);
-           use.setUser_name(user_name);
-           use.setUser_password(bCryptPasswordEncoder.encode(user_password));
-
-           accountService.add(use);
-
-
-           return "Perfect!! KO";
-       }
-       return "Done";
-   }
-
-
-
     //Post request use of the change
 
     /**
-     * Adduser method for creating and registering the user
+     * Add user method for creating and registering the user
      * @param comingM: Json response to check and save the value.
      * @return
      */
