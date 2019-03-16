cb95387bf24f88f96dcda26a58ca0b3068840d01
SoniaMahankali <mahankali.s@northeastern.edu>
2019-03-16 10:28:04 -0400

    Delete csye6225-aws-terminate-stack.sh

--- infrastructure/aws/cloudformation/csye6225-aws-terminate-stack.sh
+++
@@ -1,9 +0,0 @@
-# Delete CloudFormation Stack
-echo "CloudFormation Stack Name:"
-read name
-echo "Deleting CloudFormation Stack"
-aws cloudformation delete-stack --stack-name $name
-
-aws cloudformation wait stack-delete-complete --stack-name $name
-
-echo "Cloudformation Stack deleted"
