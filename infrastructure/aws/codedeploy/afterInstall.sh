ddb229dec6452710997ecee634272762be027926
Zhe <liu.zhe3@husky.neu.edu>
2019-03-20 11:41:16 -0400

    fix9

--- infrastructure/aws/codedeploy/afterInstall.sh
+++ infrastructure/aws/codedeploy/afterInstall.sh
@@ -3,5 +3,5 @@ cd /
 sudo cp -i /home/centos/vars.sh .
 sudo cp -i /home/centos/springbootdemo-0.0.1-SNAPSHOT.jar .
 sudo chmod +x ./vars.sh
-sudo ./vars.sh &
+sudo ./vars.sh >/dev/null 2>&1 &
 #sudo vars.sh >/dev/null 2>&1 &
