0650a4664ef5758c00a87e89bfe6036beb850c6c
Zhe <liu.zhe3@husky.neu.edu>
2019-03-20 11:33:59 -0400

    fix7

--- infrastructure/aws/codedeploy/beforeInstall.sh
+++ infrastructure/aws/codedeploy/beforeInstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#myFile="~/springbootdemo-0.0.1-SNAPSHOT.jar"
-#if [ -f "$myFile" ]; then 
-#cd ~
-#sudo rm -r springbootdemo-0.0.1-SNAPSHOT.jar 
-#pid=$(ps aux | grep "java -jar" | grep "root" | awk '{print $2}')
-#sudo kill -9 $pid 
-#fi
+myFile="~/springbootdemo-0.0.1-SNAPSHOT.jar"
+if [ -f "$myFile" ]; then 
+cd ~
+sudo rm -r springbootdemo-0.0.1-SNAPSHOT.jar 
+pid=$(ps aux | grep "java -jar" | grep "root" | awk '{print $2}')
+sudo kill -9 $pid 
+fi
